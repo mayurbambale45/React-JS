e7d5486b218e03f117b5ff9f1590a88e3d6d8c2a
mayur_bambale <mayurbambale45@email.com>
2025-10-02 16:46:08 +0530

    basics add

---
+++ 01basics/src/compo.jsx
@@ -0,0 +1,11 @@
+function Compo() {
+    return (
+        <h4>Componenet is Ready!
+            // Component file name should be with .jsx in vite. and component should be start Uppercase 
+        </h4>
+    )
+}
+
+export default Compo
+
+// Component file name should be with .jsx in vite. and component should be start Uppercase 
